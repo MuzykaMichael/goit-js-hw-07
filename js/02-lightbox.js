31b4f9ecfcc897025c58bb05d3b6a9f30d6c6938
Muzyka Mykhailo <Muzyka_Mishel@ukr.net>
2023-04-29 17:18:13 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,35 +1,33 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-const gallery = document.querySelector('.gallery')
-const imgList = galleryItems.reduce((acc, item) => {
-    return acc += `<li class="gallery__item">
-   <a class="gallery__link" href=${item.original}>
-      <img class="gallery__image" src=${item.preview} alt=${item.description} />
-   </a>
-</li>`
-},"");
-gallery.insertAdjacentHTML('beforebegin',imgList)
-const galleryItem = gallery.querySelector('.gallery__item')
-gallery.style.listStyle = "none";
-function showModal(event) {
-    event.preventDefault();
-    if (event.target.nodeName !== "IMG"){
-        return;
-    } else {
-        console.log(event.target.dataset.source)
-        const instance = $(`<div class="gallery">
-        <a href="images/image1.jpg"><img src="images/thumbs/thumb1.jpg" alt="" title=""/></a>
-        <a href="images/image2.jpg"><img src="images/thumbs/thumb2.jpg" alt="" title="Beautiful Image"/></a>
-        <a href="images/image1.jpg"><img src="images/thumbs/thumb1.jpg" alt="" title=""/></a>
-        <a href="images/image2.jpg"><img src="images/thumbs/thumb2.jpg" alt="" title="Beautiful Image"/></a>
-        <a href="images/image1.jpg"><img src="images/thumbs/thumb1.jpg" alt="" title=""/></a>
-        <a href="images/image2.jpg"><img src="images/thumbs/thumb2.jpg" alt="" title="Beautiful Image"/></a>
-        <a href="images/image1.jpg"><img src="images/thumbs/thumb1.jpg" alt="" title=""/></a>
-        <a href="images/image2.jpg"><img src="images/thumbs/thumb2.jpg" alt="" title="Beautiful Image"/></a>
-        <a href="images/image1.jpg"><img src="images/thumbs/thumb1.jpg" alt="" title=""/></a>
-        <a href="images/image2.jpg"><img src="images/thumbs/thumb2.jpg" alt="" title="Beautiful Image"/></a>
-    </div>`).simpleLightbox({});
-    }
-}
-
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+const galleryContainer = createGalleryItems(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend',galleryContainer);
+
+gallery.addEventListener('click',zoomGalleryItems);
+
+function createGalleryItems(galleryItems) {
+    return galleryItems.map(({description,original,preview})=> {
+    return`<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+       <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+ </li>`;}).join('');
+   }
+
+
+function zoomGalleryItems(e){
+e.preventDefault();
+const isImgEl = e.target.classList.contains('gallery__image');
+if(!isImgEl){
+    return
+} 
+var lightbox = new SimpleLightbox('.gallery__link',{
+captionsData: 'alt',
+captionDelay: 250,
+});
+
+}
